3921628bad95f9be1c698b46abe6f0da7b557096
Eric Greene <eric@t4d.io>
2021-03-19 10:14:13 -0400

    add doc content

--- calc-app/models/history.py
+++ calc-app/models/history.py
@@ -1,3 +1,14 @@
+"""Provides the History class.
+
+The History class is the primary data structure of the Calc App program. It
+stores the history of math operations performed with the calculator.
+
+  Typical usage example:
+
+  history = History(calc_ops)<br>
+  history.append("add", 2.0)
+"""
+
 from functools import reduce
 
 from mixins.json_mixin import JsonMixin
@@ -5,24 +16,44 @@ from models.history_entry import HistoryEntry
 
 
 class History(JsonMixin):
+    """ Manages the operation history
+
+    Attributes:
+        _entries: A list of math operation entries.
+        _calc_ops: The list of math operations configured for the calculator.
+    """
 
     def __init__(self, calc_ops):
         self._entries = []
         self._calc_ops = calc_ops
 
     def append(self, entry_op_name, entry_op_value):
+        """
+        Appends a new history entry to the list of entries.
+
+        Args:
+            entry_op_name (str): The name of the operation.
+            entry_op_value (float): The value of the operation.
+
+        Returns:
+            History: Returns an instance to self.
+        """
 
         next_entry_id = max([entry.id for entry in self._entries] or [0]) + 1
 
         new_history_entry = HistoryEntry(
             next_entry_id, entry_op_name, entry_op_value)
         self._entries.append(new_history_entry)
+        return self
 
     def remove(self, entry_id):
+
         for entry in self._entries:
             if entry.id == entry_id:
                 self._entries.remove(entry)
                 break
+
+        return self
 
     def clear(self):
         self._entries.clear()
